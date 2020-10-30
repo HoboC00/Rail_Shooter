7df8c6ceee2fcec9c8137d0410e36be3fa2ba05a
Aman Srivastava <aman15220@gmail.com>
2020-10-30 18:47:35 +0530

    playerControl

---
+++ Rail_Shooter/Assets/CollisionHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        StartDeathSequence();
+    }
+
+    private void StartDeathSequence()
+    {
+        print("Player dying");
+        SendMessage("OnPlayerDeath");
+
+    }
+}
--- Rail_Shooter/Assets/PlayerController.cs
+++ Rail_Shooter/Assets/PlayerController.cs
@@ -3,49 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
-public class Player : MonoBehaviour
-{
-    [Tooltip("In ms^-1")] [SerializeField]  float Speed = 4f;
-    [Tooltip("In m")] [SerializeField]  float xRange = 5f;
+public class PlayerController : MonoBehaviour
+{   [Header("General")]
+    [Tooltip("In ms^-1")] [SerializeField] float Speed = 4f;
+    [Tooltip("In m")] [SerializeField] float xRange = 5f;
     [Tooltip("In m")] [SerializeField] float yRange = 3f;
 
+    [Header("Screen-position Based")]
     [SerializeField] private float positionPitchFactor = -5f;
     [SerializeField] private float positionYawFactor = 5f;
+
+
+    [Header("Control-Throw Based")]
+    [SerializeField] private float controlPitchFactor = -20f;
     [SerializeField] private float controlRollFactor = -20f;
 
-    [SerializeField] private float controlPitchFactor = -20f;
-
     float xThrow, yThrow;
+
+     bool isControlEnabled = true;
     // Start is called before the first frame update 
-    void Start()
-    {
-        
-    }
+
+
+
 
     // Update is called once per frame
     void Update()
     {
-        ProcessTranslation();
-        ProcessRotation();
+        if (isControlEnabled)
+        {
+            ProcessTranslation();
+            ProcessRotation();
+        }
     }
+
+    void OnPlayerDeath()
+    {
+        isControlEnabled = false;
+    }
+
+
 
     private void ProcessRotation()
     {
         float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
         float pitchDueToControlThrow = yThrow * controlPitchFactor;
-        float pitch = pitchDueToPosition + pitchDueToControlThrow; 
-        
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
         float yaw = transform.localPosition.x * positionYawFactor;
-        
+
         float roll = xThrow * controlRollFactor;
-        
-        transform.localRotation = Quaternion.Euler(pitch,yaw,roll);
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 
     private void ProcessTranslation()
     {
-         xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-         yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
 
         float xOffset = xThrow * Speed * Time.deltaTime;
         float yOffset = yThrow * Speed * Time.deltaTime;
